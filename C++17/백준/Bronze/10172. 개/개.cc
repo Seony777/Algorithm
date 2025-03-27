724224915638ef15a534369e4f63c810e59837a7
Seony777 <addiolsw1@gmail.com>
2025-03-28 01:09:19 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"|\\_/|"<<endl;	// \는 이스케이프 문자로 \\와 같이 두번써야 문자열로 출력
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	
+
+	return 0;
+}
