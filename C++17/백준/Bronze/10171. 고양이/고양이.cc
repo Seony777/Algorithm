a7beb2b0b7d3afaee7287543ae0f00a1200c6e01
Seony777 <addiolsw1@gmail.com>
2025-03-28 00:33:17 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\\    /\\"<<endl;	// \는 이스케이프 문자로 \\와 같이 두번써야 문자열로 출력
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	
+
+	return 0;
+}
