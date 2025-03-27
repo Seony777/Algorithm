7a33b52630eb362786b6b5f13388abfd63e88dc9
Seony777 <addiolsw1@gmail.com>
2025-03-28 00:26:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long a, b,c;	// a,b,c가 10**12까지 가능하므로 큰수는 왠만하면 long long을 쓰자
+
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	
+
+	return 0;
+}
