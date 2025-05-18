ccb953e4d587265c8dc3041a504b5e3f68b4f566
Seony777 <addiolsw1@gmail.com>
2025-05-18 22:34:54 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+int M, N;
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> M >> N;
+	
+	for (int i = sqrt(M); i <= sqrt(N); i++)
+	{
+		if (i * i >= M && i * i <= N)
+		{
+			v.push_back(i * i);
+		}
+	}
+
+	int sum = 0;
+	for (auto n : v)
+	{
+		sum += n;
+	}
+	if (v.size() == 0)
+	{
+		cout << -1;
+	}
+	else {
+		cout << sum << '\n' << v[0];
+	}
+}
